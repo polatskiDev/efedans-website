48ac6165d615978bb75127a1583553702543502c
Orhan Polat <60147495+polatskiDev@users.noreply.github.com>
2026-07-29 16:59:04 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -36,6 +36,9 @@ const nextConfig: NextConfig = {
       {
         source: "/:path*",
         headers: securityHeaders,
+        images: {
+          unoptimized: true,
+        },
       },
     ];
   },
